781fe44caebd38df15e9152e897a71cb4cd4a718
Nicholas1514 <121171918+Nicholas1514@users.noreply.github.com>
2025-01-05 14:17:23 +0100

    Add files via upload
    aggiunta header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionale di un bar</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="dashboard.php">Gestionale BAR</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="products.php">Prodotti</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sales.php">Vendite</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="alerts.php">Soglie Allerta</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="container mt-4">
