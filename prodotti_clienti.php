01f603cdf0d5d05f0ab0bbc39ccb1046e369d605
Nicholas1514 <121171918+Nicholas1514@users.noreply.github.com>
2025-01-16 08:16:47 +0100

    Add files via upload

---
+++ prodotti_clienti.php
@@ -0,0 +1,77 @@
+<?php
+include 'headerU.php';
+include 'connessione.php';
+session_start();
+
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$query = "SELECT * FROM prodotti";
+$result = $conn->query($query);
+?>
+<html>
+<head>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+ <style>
+        body {
+            background: linear-gradient(to bottom right, #6a11cb, #2575fc);
+            color: black;
+            font-family: Arial, sans-serif;
+        }
+        h3
+        {
+        	font-weight: bold;
+            text-align: center;
+            text-decoration: underline;
+		}
+        .container {
+            background: rgba(255, 255, 255, 0.1);
+            padding: 30px;
+            border-radius: 10px;
+            margin-top: 50px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+        }
+        .form-control {
+            background: rgba(255, 255, 255, 0.2);
+            border: none;
+            color: white;
+        }
+        .form-control::placeholder {
+            color: rgba(255, 255, 255, 0.7);
+        }
+        .btn-primary {
+            background-color: green;
+            border: none;
+        }
+        .btn-primary:hover {
+            background-color: limegreen;
+        }
+    </style>
+
+<head>
+<div class = "container">
+<h2>Prodotti</h2>
+
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+          
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+                <td><?php echo $row['ID']; ?></td>
+                <td><?php echo $row['nome']; ?></td>
+              
+              
+        <?php } ?>
+    </tbody>
+</table>
+</div>
+<html>
